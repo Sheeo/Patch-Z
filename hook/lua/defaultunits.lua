86e33a4efbb3d1aec12c3cac46618a2cc8cd8357
aeoncleanse <ben-knight@bluebottle.com>
2014-08-20 20:18:19 +0100

    Next step for adjacency fix

--- hook/lua/defaultunits.lua
+++ hook/lua/defaultunits.lua
@@ -32,13 +32,50 @@ MassFabricationUnit = Class(StructureUnit) {
 			local adjBuffs = self:GetBlueprint().Adjacency
 			if not adjBuffs then return end
 		
+			--Apply all buffs to all adjacent units. Like this?
 			for k,v in AdjacencyBuffs[adjBuffs] do
-				for k,v in AdjacentUnitMemory do
+				for k,adjacentUnit in AdjacentUnitMemory do
 					Buff.ApplyBuff(adjacentUnit, v, self)
 				end
 			end
+			
+			--Turn on the visual adjacency FX
+			for k,adjacentUnit in AdjacentUnitMemory do
+				CreateAdjacentEffect(self, v)		--Function found in StructureUnit class
+				adjacentUnit:RequestRefreshUI()				
+			end
+			
 			self:RequestRefreshUI()
-			adjacentUnit:RequestRefreshUI()
+		end
+    end,
+	
+	OnConsumptionInActive = function(self)
+        StructureUnit.OnConsumptionInActive(self)
+        self:SetMaintenanceConsumptionInactive()
+        self:SetProductionActive(false)
+		
+		--Remove any active adjacency bonuses provided by this unit
+		local asjBuffs = self:GetBlueprint().Adjacency
+		if adjBuffs and AdjacencyBuffs[adjBuffs] then		--Make sure there are some buffs present
+			for k,v in AdjacencyBuffs[adjBuffs] do
+				for k,adjacentUnit in AdjacentUnitMemory do
+					if Buff.HasBuff(adjacentUnit, v) then
+						Buff.RemoveBuff(adjacentUnit, v)
+					end
+				end
+			end
+			
+			--Turn off the visual adjacency FX
+			for k,adjacentUnit in AdjacentUnitMemory do
+				DestroyAdjacentEffects(self, v)
+				adjacentUnit:RequestRefreshUI()
+			end
+			
+			self:RequestRefreshUI()
 		end
     end,
 }
+
+
+
+
