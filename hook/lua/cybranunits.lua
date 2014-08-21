60965ce82b0fa237b34643216889fa8bf2420a9b
aeoncleanse <ben-knight@bluebottle.com>
2014-08-21 15:53:38 +0100

    Begin to fix Factory Rolloffs

---
+++ hook/lua/cybranunits.lua
@@ -0,0 +1,51 @@
+local oldCAirFactoryUnit = CAirFactoryUnit
+CAirFactoryUnit = Class(oldCAirFactoryUnit) {
+
+    FinishBuildThread = function(self, unitBeingBuilt, order )
+        self:SetBusy(true)
+        self:SetBlockCommandQueue(true)
+        local bp = self:GetBlueprint()
+        local bpAnim = bp.Display.AnimationFinishBuildLand
+        if bpAnim and EntityCategoryContains(categories.LAND, unitBeingBuilt) then
+            self.RollOffAnim = CreateAnimator(self):PlayAnim(bpAnim):SetRate(5) --Increase the Rate
+            self.Trash:Add(self.RollOffAnim)
+--            WaitTicks(1)				--Try removing this to get that last bit of speed
+            WaitFor(self.RollOffAnim)
+        end
+        if unitBeingBuilt and not unitBeingBuilt:IsDead() then
+            unitBeingBuilt:DetachFrom(true)
+        end
+        self:DetachAll(bp.Display.BuildAttachBone or 0)
+        self:DestroyBuildRotator()
+        if order != 'Upgrade' then
+            ChangeState(self, self.RollingOffState)
+        else
+            self:SetBusy(false)
+            self:SetBlockCommandQueue(false)
+        end
+    end,
+
+    PlayFxRollOffEnd = function(self)
+        if self.RollOffAnim then        
+            self.RollOffAnim:SetRate(-5)	--Increase the Rate
+            WaitFor(self.RollOffAnim)
+            self.RollOffAnim:Destroy()
+            self.RollOffAnim = nil
+        end
+    end,	
+	
+    RolloffBody = function(self)
+        self:SetBusy(true)
+        self:SetBlockCommandQueue(true)
+        self:PlayFxRollOff()
+        # Wait until unit has left the factory
+        while not self.UnitBeingBuilt:IsDead() and self.MoveCommand and not IsCommandDone(self.MoveCommand) do
+            WaitSeconds(0.1)				--Decrease the check interval (0.5)
+        end
+        self.MoveCommand = nil
+        self:PlayFxRollOffEnd()
+        self:SetBusy(false)
+        self:SetBlockCommandQueue(false)
+        ChangeState(self, self.IdleState)
+    end,	
+}
